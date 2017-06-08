8666a10dff30230799e23b9a73a17658d915fd49
Fabien Moritz <fabien.moritz@gmail.com>
2017-06-08 09:29:27 +0200

    coroutines avec jooby

--- jooby/src/main/kotlin/Main.kt
+++ jooby/src/main/kotlin/Main.kt
@@ -1,10 +1,30 @@
+import kotlinx.coroutines.experimental.CommonPool
+import kotlinx.coroutines.experimental.delay
+import kotlinx.coroutines.experimental.future.future
+import kotlinx.coroutines.experimental.runBlocking
+import org.jooby.AsyncMapper
 import org.jooby.run
+import java.util.concurrent.TimeUnit
 
 fun main(args:Array<String>) {
     run(*args) {
-        get {
+
+        map(AsyncMapper())
+        get("/coroutineDelay") {
+            future(CommonPool) {
+                delay(1, TimeUnit.SECONDS)
+                "Hello World!"
+            }
+        }
+        get("/threadSleep") {
             Thread.sleep(1000)
             "Hello World!"
+        }
+        get("/coroutineBlocking") {
+            runBlocking(CommonPool) {
+                delay(1, TimeUnit.SECONDS)
+                "Hello World!"
+            }
         }
     }
 }
